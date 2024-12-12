71d6c3ef10117e9df6ccacf6f98a7976ee74f80b
Ananda Risyad <anandarisyad@gmail.com>
2024-12-12 10:05:17 +0700

    AppPage

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,17 @@ import './App.css'
 import { LandingPage } from './pages/LandingPage';
 import { LoginPage } from './pages/LoginPage';
 import { DashboardPage } from './pages/DashboardPage';
+import ProtectedRoute from './components/ProtectedRoute';
+import AuthService from './services/AuthService';
+import { AppPage } from './pages/AppPage';
 
 
 function App() {
+  const [auth, setAuth] = useState(false);
+
+  useEffect(() => {
+    setAuth(AuthService.isAuthenticated())
+  }, [])
 
   return (
     <>
@@ -15,6 +23,8 @@ function App() {
           <Route path="/" element={<LandingPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/dashboard" element={<DashboardPage />} />
+
+          <Route path="/app" element={<ProtectedRoute isAuthenticated={auth}> <AppPage /> </ProtectedRoute>}
 
         </Routes>
 
